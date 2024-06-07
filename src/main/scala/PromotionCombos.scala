40e17a0133d0bdba7c37d3c652fa1df9031220cb
Riuga <riuga@tuta.io>
2024-06-07 05:18:08 -0500

    Fix regressions detected in promo combo property tests
    We needed to:
    
    1) sanitize the inputs
    2) not progressively exclude past root promotions in the fold
    3) handle backwards compatibility dead-end cases differently

--- src/main/scala/PromotionCombos.scala
+++ src/main/scala/PromotionCombos.scala
@@ -1,7 +1,17 @@
 package promotion_combos
 
+import scala.util.chaining.scalaUtilChainingOps
+
 case class Promotion(code: String, notCombinableWith: Seq[String])
 
+// We leverage the fact that we are defining a type that wraps Seq[String]
+// and can define our own instances (overrides) of certain traits.
+//
+// In this particular case, within the bounds of the assignment,
+// it makes sense for a PromotionCombo to have special equality
+// properties. Namely, that it is set-like. The ordering of the
+// internal promotion codes does not matter for two PromotionCombos 
+// to be equal.
 case class PromotionCombo(promotionCodes: Seq[String]) {
   override def equals(that: Any) =
     that match {
@@ -9,80 +19,89 @@ case class PromotionCombo(promotionCodes: Seq[String]) {
       case _ => false
     }
   
+  // We can leverage bitwise operator laws to splice hashcodes in a manner that doesn't
+  // respect ordering.
+  //
+  // Probably a more idiomatic way of doing this though; just followed
+  // my intuition here.
+  //
+  // Necessary since Set may default to HashSet and hashCodes will be compared before `equals`
   override def hashCode() =
-    this.promotionCodes.foldLeft(0x0)(_.hashCode ^ _.hashCode)
+    this.promotionCodes.foldLeft(0)(_.hashCode ^ _.hashCode)
 }
 
-
-
-// One major consideration we need to make here is what are
-// the guarantees of our input dataset `allPromotions`?
-//
-// Consider the following input set edge case:
-//    Promotion("P1", Seq.empty)
-//    Promotion("P2", Seq("P1"))
-//
-// In an ideal world the input data set is "clean" and in such
-// a scenario, instead of Seq.empty it should also specify that 
-// it is not combinable with P2.
-//
-// Our ideal implementation of this function is going to vary
-// based on whether or not such an invariant holds:
-//
-// 1. Assuming the invariant holds, we can recurse through the
-// set of all unique promotions knowing only what remains to be 
-// be potentially appended to the end of a PromotionCombo 
-// sequence, and what the current Promotion is.
-//
-// 2. If the invariant does not hold, we need to also have 
-// "backwards" knowledge of what has been appended so far.
-// This increases space requirements; but we can limit it by
-// recursing this parameter as a View (read-only reference).
-//
-// As such an invariant on the input data was not specified,
-// we should play it safe and assume that our input dataset
-// is inconsistent; especially if it's coming from a third
-// party API provider that we can't audit.
 def allCombinablePromotions(allPromotions: Seq[Promotion]): Seq[PromotionCombo] =
-  // Recursively find combinable chains of promotions by starting from an initial promotion
+  // Recursively find combinable chains of promotions, starting from an initial promotion
   // and branching into compatible (combinable) remaining promotions.
   def findPromotionChains(
     promotionCodesInChain: Set[String],
     promotionToAdd: Promotion,
     remainingPromotions: Set[Promotion]
-  ): Set[PromotionCombo] = {
-    def isForwardsCompatible(p : Promotion) : Boolean =
-      !promotionToAdd.notCombinableWith.contains(p.code)
-
+  ): Option[Set[PromotionCombo]] = {
     val isBackwardsCompatible = 
       (promotionCodesInChain & promotionToAdd.notCombinableWith.toSet).isEmpty
 
-    lazy val remainingCompatiblePromotions = 
-      remainingPromotions.filter(isForwardsCompatible(_))
-
-    // Base case #1: End the chain and don't append the current promotion if it isn't backwards compatible.
+    // Base case #1: End the chain as a dead end (None) if the current promotion isn't backwards compatible
+    // with what is already in the chain.
     if !isBackwardsCompatible then
-      return Set(PromotionCombo(promotionCodesInChain.toSeq))
+      return None
 
-    // Base case #2: End the chain and append the current promotion if there are no more remaining promotions to traverse.
-    if remainingCompatiblePromotions.isEmpty then
-      return Set(PromotionCombo((promotionCodesInChain + promotionToAdd.code).toSeq))
+    val forwardsCompatiblePromotions = 
+      remainingPromotions.filter(p => !promotionToAdd.notCombinableWith.contains(p.code))
+
+    // Base case #2: End the chain and return its promotion codes (inclusive of the current promotion) if 
+    // there are no more remaining promotions that are be compatible with the chain to traverse.
+    if forwardsCompatiblePromotions.isEmpty then
+      return Some(Set(PromotionCombo((promotionCodesInChain + promotionToAdd.code).toSeq)))
 
     // Recursive case: Explore the remaining combinable promotions as individual chains and flatten
     // the resulting chains (sets).
-    remainingCompatiblePromotions.flatMap(p => 
-      findPromotionChains(
-        promotionCodesInChain + promotionToAdd.code, 
-        p, 
-        remainingCompatiblePromotions.excl(p)
-      )
-    )
+    //
+    // If all paths dead-end (i.e. all are None), return the current chain up to this point (inclusive)
+    //
+    // N.B. I would have liked to use something like Haskell's `sequence` here, but apparently this only exists in ScalaZ.
+    // I didn't want to add a dependency just for it so this is slightly less than ideal way of expressing this.
+    val subchains =
+      forwardsCompatiblePromotions
+        .flatMap(p => 
+          findPromotionChains(
+            promotionCodesInChain + promotionToAdd.code, 
+            p, 
+            forwardsCompatiblePromotions.excl(p)
+          )
+        )
+        .flatten
+    
+    if subchains.isEmpty then 
+      Some(Set(PromotionCombo((promotionCodesInChain + promotionToAdd.code).toSeq)))
+    else Some(subchains)
   }
 
-  allPromotions.foldLeft((Set.empty : Set[PromotionCombo], allPromotions.toSet)){ case ((accum, possibilities), p) =>
-    val filteredPossibilities = possibilities.excl(p)
-    (accum.union(findPromotionChains(Set.empty, p, filteredPossibilities)), filteredPossibilities)
-  }._1.toSeq.filter(_.promotionCodes.length > 1)
+  val sanitizedPromotions = {
+    // There may be duplicate promotions in the input; we want to combine their notCombinableWiths
+    // to err on the side of caution
+    def mergeDuplicates(xs: Seq[Promotion]) = 
+      xs.groupMapReduce(_.code)(x => x)((x, y) => 
+        Promotion(x.code, x.notCombinableWith ++ y.notCombinableWith)
+      ).values
+
+    // There may be self-conflicting Promotions in the input (i.e. a promotion with its own code in the notCombinableWith list)
+    // We want to disregard these entirely.
+    def discardSelfNoncombinables(xs: Iterable[Promotion]) = 
+      xs.filter(p => !p.notCombinableWith.contains(p.code))
+    
+    allPromotions
+      .pipe(mergeDuplicates)
+      .pipe(discardSelfNoncombinables)
+      .toSet
+  }
+
+  sanitizedPromotions.foldLeft(Set.empty : Set[PromotionCombo]) { (accum, p) =>
+    accum.union(
+      findPromotionChains(Set.empty, p, sanitizedPromotions.excl(p))
+        .getOrElse(Set.empty)
+    )
+  }.toSeq.filter(_.promotionCodes.length > 1)
 
 def combinablePromotions(promotionCode: String, allPromotions: Seq[Promotion]): Seq[PromotionCombo] =
   allCombinablePromotions(allPromotions)
